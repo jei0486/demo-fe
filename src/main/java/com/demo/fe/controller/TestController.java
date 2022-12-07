a1ad6bf558aa49e99acc46308f81e68e17956caf
jinseul <jei0486@gmail.com>
2022-12-07 11:23:47 +0900

    - k8s server port 8080 으로 변경 - application profile 설정

--- src/main/java/com/demo/fe/controller/TestController.java
+++ src/main/java/com/demo/fe/controller/TestController.java
@@ -8,8 +8,6 @@ import reactor.netty.http.server.HttpServerRequest;
 @RestController
 public class TestController {
 
-    public static final String TOKEN_HEADER = "Authorization";
-
     @GetMapping("/boards/fail")
     public void doFail(){
         throw new RuntimeException("faild");
